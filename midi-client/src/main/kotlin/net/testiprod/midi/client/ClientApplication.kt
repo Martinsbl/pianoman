ac2c9a215b96ffd6f96479faa645c104d6680b6d
martin <martinsbl@gmail.com>
2025-06-23 18:26:11 +0200

    send midi messages

--- midi-client/src/main/kotlin/net/testiprod/midi/client/ClientApplication.kt
+++ midi-client/src/main/kotlin/net/testiprod/midi/client/ClientApplication.kt
@@ -3,14 +3,17 @@ package net.testiprod.midi.client
 import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
+import net.testiprod.pianoman.transport.TMidiMessage
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("MidiClient")
 
 fun main() {
     val midiHttpClient = MidiHttpClient("http://127.0.0.1", 8080)
+    val midiWebSocketClient = MidiWebSocketClient("ws://localhost", 8080)
 
     val scope = CoroutineScope(Dispatchers.Default)
     logger.info("Hello from Midi Client!")
@@ -20,7 +23,19 @@ fun main() {
 //            is NetworkResult.Error -> logger.info("Error: $result")
 //        }
 //    }
-    val midiWebSocketClient = MidiWebSocketClient("ws://localhost", 8080)
+
+    logger.info("Attempting to connect to WebSocket...")
+    midiWebSocketClient.connectToTransmitWebSocket(1258500461, scope)
+    logger.info("WebSocket connected successfully")
+    scope.launch {
+        repeat(5) {
+            val message = TMidiMessage(144, 108, 40)
+            logger.info("Sending MIDI message: $message")
+            midiWebSocketClient.sendMidiMessage(message)
+            delay(1000) // Wait for a second before sending the next message
+        }
+    }
+
     scope.launch {
         withTimeout(5.seconds) {
             val flow = midiWebSocketClient.getMidiMessageFlow(873200140)
--- midi-client/src/main/kotlin/net/testiprod/midi/client/MidiWebSocketClient.kt
+++ midi-client/src/main/kotlin/net/testiprod/midi/client/MidiWebSocketClient.kt
@@ -6,11 +6,17 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.websocket.pingInterval
 import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.utils.io.core.Closeable
 import io.ktor.websocket.Frame
 import io.ktor.websocket.readText
 import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import net.testiprod.midi.client.ktor.commonGson
 import net.testiprod.midi.client.ktor.configureGson
 import net.testiprod.pianoman.transport.TMidiMessage
@@ -18,9 +24,7 @@ import org.slf4j.LoggerFactory
 
 class MidiWebSocketClient(
     private val client: HttpClient
-) {
-
-    private val logger = LoggerFactory.getLogger(javaClass)
+) : Closeable {
 
     constructor(host: String, port: Int) : this(
         HttpClient(CIO) {
@@ -34,8 +38,40 @@ class MidiWebSocketClient(
         },
     )
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    private val sendChannel = Channel<String>(Channel.UNLIMITED)
+    private var sessionJob: Job? = null
+
+    fun connectToTransmitWebSocket(deviceId: Int, scope: CoroutineScope) {
+        logger.info("Connecting to TX WebSocket for device $deviceId")
+        sessionJob = scope.launch(Dispatchers.IO) {
+            client.webSocket("/midi/devices/$deviceId/ws-send") {
+                try {
+                    for (message in sendChannel) {
+                        send(Frame.Text(message))
+                    }
+                } catch (e: Exception) {
+                    logger.error("Error sending message to TX WebSocket: ${e.message}", e)
+                } finally {
+                    client.close()
+                    logger.info("TX WebSocket connection closed for device $deviceId")
+                }
+            }
+        }
+    }
+
+    suspend fun sendMidiMessage(message: TMidiMessage) {
+        if (sessionJob?.isActive == true) {
+            val json = commonGson.toJson(message)
+            sendChannel.send(json)
+        } else {
+            logger.warn("Session is not active, cannot send MIDI message for device")
+        }
+    }
+
     fun getMidiMessageFlow(deviceId: Int): Flow<TMidiMessage> = flow {
-        logger.info("Connecting to WebSocket for device $deviceId")
+        logger.info("Connecting to RX WebSocket for device $deviceId")
         try {
             client.webSocket("/midi/devices/$deviceId/ws-receive") {
                 for (frame in incoming) {
@@ -51,11 +87,16 @@ class MidiWebSocketClient(
                 }
             }
         } catch (e: Exception) {
-            logger.error(e.message, e)
+            logger.error("Error connecting to RX WebSocket for device $deviceId: ${e.message}", e)
         } finally {
             client.close()
-            logger.info("WebSocket connection closed for device $deviceId")
+            logger.info("RX WebSocket connection closed for device $deviceId")
         }
+    }
+
+    override fun close() {
+        sendChannel.close()
+        sessionJob?.cancel()
     }
 
 }
